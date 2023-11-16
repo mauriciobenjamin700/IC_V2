8dcb768305337db64ef48ff6e4cc6150d0341606
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2023-11-16 10:45:48 -0300

    Estrutura do Nodelo Encaminhado

--- YOLO/teste.py
+++ YOLO/teste.py
@@ -1,1 +1,16 @@
-from ultralytics import yolo
+from ultralytics import YOLO
+import os
+import shutil
+
+#model = YOLO('yolov8s.yaml')
+
+try:
+    model = YOLO('model_test/weights/best.pt',task='segment')
+    if os.path.exists('runs'):
+        shutil.rmtree('runs')
+    
+    model.predict('1.jpg',save=True,conf=0.5)
+except:
+    print("falhei")
+
+
